bfa1866ffeea57aa2e8c81eb26861398f4479c2f
Soham Dandapath <soham.dandapath@gmail.com>
2023-04-27 16:42:00 -0400

    plot changes

--- util.py
+++ util.py
@@ -4,15 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_image(orig, fake_img, epoch, num_imgs = 10, plot_dir = 'plot/'):
-    _, ax = plt.subplots(2, num_imgs, figsize=(24,8))
+    _, ax = plt.subplots(2, num_imgs, figsize=(24,4))
     with torch.no_grad():
         orig = np.transpose(orig.cpu().numpy(), (0, 2, 3, 1))
         for i in range(num_imgs):
             ax[0][i].imshow(orig[i])
+            ax[0][i].xticks([])
+            ax[0][i].yticks([])
 
         fake_img = np.transpose(fake_img.cpu().numpy(), (0, 2, 3, 1))
         for i in range(num_imgs):
             ax[1][i].imshow(fake_img[i])
+            ax[1][i].xticks([])
+            ax[1][i].yticks([])
     plt.savefig(f"{plot_dir}{epoch}.jpg")
 
 def save_model(bgm, disc, epoch, model_dir = 'saved_model/'):
